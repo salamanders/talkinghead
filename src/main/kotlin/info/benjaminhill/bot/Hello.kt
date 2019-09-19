22e95ecb4d9a3d4da9d835ea17fd42fb61766aaa
Benjamin Hill <benjaminhill@gmail.com>
2019-09-19 13:24:38 -0400

    Blank project

---
+++ src/main/kotlin/info/benjaminhill/bot/Hello.kt
@@ -0,0 +1,6 @@
+package info.benjaminhill.bot
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/info/benjaminhill/bot/HelloTest.kt
@@ -0,0 +1,8 @@
+package info.benjaminhill.bot
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
